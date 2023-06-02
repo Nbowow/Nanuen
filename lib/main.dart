735719f43c4d8e06bfb87a058bd142c8a3aff83e
Nbowow <nbo5548@naver.com>
2023-06-02 17:08:06 +0900

    Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_user.dart';
 import 'package:nanuen_sns_login/chat_screen.dart';
+import 'package:nanuen_sns_login/screen/main_screen.dart';
 
 void main() async {
   KakaoSdk.init(nativeAppKey: '76e93a7e6dad66a0b2d3f68f72ae66e7');
@@ -27,39 +28,40 @@ class MyApp extends StatelessWidget {
       ),
       //home: const App(),
       //home: const KakaoMain(),
-      home: const LoginPage(),
+      //home: const LoginPage(),
+      home: LoginSignupScreen(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Authentication(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Authentication(),
+//     );
+//   }
+// }
 
-class Authentication extends StatelessWidget {
-  const Authentication({super.key});
+// class Authentication extends StatelessWidget {
+//   const Authentication({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance
-          .authStateChanges(), //�궗�슜�옄媛� 濡쒓렇�씤/濡쒓렇�븘�썐�쓣 �븷�븣留덈떎 state媛� 蹂��솕�븯怨� builder硫붿냼�뱶瑜� �넻�빐 �깉濡�寃� �솕硫� 洹몃━湲� 媛��뒫
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          //data �뾾�쓣�븣 > 濡쒓렇�씤 �솕硫�
-          return const SignInScreen(
-            providerConfigs: [EmailProviderConfiguration()],
-          );
-        }
-        return const ChatScreen();
-      },
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder(
+//       stream: FirebaseAuth.instance
+//           .authStateChanges(), //�궗�슜�옄媛� 濡쒓렇�씤/濡쒓렇�븘�썐�쓣 �븷�븣留덈떎 state媛� 蹂��솕�븯怨� builder硫붿냼�뱶瑜� �넻�빐 �깉濡�寃� �솕硫� 洹몃━湲� 媛��뒫
+//       builder: (context, snapshot) {
+//         if (!snapshot.hasData) {
+//           //data �뾾�쓣�븣 > 濡쒓렇�씤 �솕硫�
+//           return const SignInScreen(
+//             providerConfigs: [EmailProviderConfiguration()],
+//           );
+//         }
+//         return const ChatScreen();
+//       },
+//     );
+//   }
+// }
